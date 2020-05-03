a8daad9d886879b092727994b6b40074f70e6834
Furqan Butt <furqan.butt@northbaysolutions.net>
2020-05-04 04:05:19 +0500

    added dataset file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 APP_NAME = 'GOT dataset Parquest Conversion'
-FILE_PATH = 'file:///Users/Lenovo/Desktop/Furqan Work/PySpark Training/corona_analysis/corona_country_dataset/covid_19_data.csv'
+FILE_PATH = '<file_path>'
 ROW_NUM = 100
 
